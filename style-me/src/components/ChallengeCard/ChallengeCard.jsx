b64d4973242e8c989e5c1fd46ad6249af5fed260
Guilherme Girata <guilherme_girata@hotmail.com>
2024-03-01 21:43:16 -0300

    challenge card component

--- style-me/src/components/ChallengeCard/ChallengeCard.jsx
+++ style-me/src/components/ChallengeCard/ChallengeCard.jsx
@@ -5,10 +5,19 @@ export const ChallengeCard = () => {
   return (
     <div className='ChallengeCardContainer'>
       <div className='ChallengeCard'>
-        <div className='Card' />
+        <div className='Card'>
+        <span className='ChallengeNumber'>#1</span>
+          <div className='CardInfoContainer'>
+            <div className='CardSideColor' />
+            <div className='CardTitle'>
+              <h1>Alterar a cor de um texto</h1>
+              <span>Um texto está difícil de ser visualizado em uma página, faça com que o usuário consiga ler o texto facilmente. </span>
+              <button className="ChallengeButton">VISUALIZAR</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  
   );
 };
 
