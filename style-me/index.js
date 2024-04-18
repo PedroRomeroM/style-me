b42ea726a31ad4c75a689489570431a22d2f1863
PedroRomeroM <122692587+PedroRomeroM@users.noreply.github.com>
2024-04-17 21:59:46 -0300

    teste

--- style-me/index.js
+++ style-me/index.js
@@ -1,28 +1,29 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const httpProxy = require('express-http-proxy');
-var http = require('http');
-var logger = require('morgan');
-const port = 3000;
-const express = require('express')
+const http = require('http');
+const logger = require('morgan');
 const helmet = require('helmet');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
 
+const port = 3000;  
 const userServiceProxy = httpProxy(process.env.REACT_APP_API_GATEWAY);
 
-// USER
-app.post(`/user`, (req, res, next) => userServiceProxy(req, res, next));
-
-
-// PUBLICACAO
-app.use(logger('dev'));
-app.use(cors());
+app.use(logger('combined'));
+app.use(cors({
+  origin: 'http://localhost:3001',  
+  optionsSuccessStatus: 200 
+}));
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.post(`/user`, (req, res, next) => userServiceProxy(req, res, next));
+
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
