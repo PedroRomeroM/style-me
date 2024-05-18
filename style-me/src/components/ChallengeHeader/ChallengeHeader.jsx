2cabd3692e9c637484b1e7df019ed33929194454
Guilherme Girata <guilherme_girata@hotmail.com>
2024-05-18 13:09:19 -0300

    challenges homepage fix - carrousel removal

--- style-me/src/components/ChallengeHeader/ChallengeHeader.jsx
+++ style-me/src/components/ChallengeHeader/ChallengeHeader.jsx
@@ -1,20 +1,38 @@
-import './ChallengeHeader.scss'
+import './ChallengeHeader.scss';
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
-export const ChallengeHeader = ({ color, difficulty }) => {
+const ChallengeHeader = ({ color, difficulty, selectedDifficulty, onSelectDifficulty }) => {
   return (
     <div className='ChallengeHeaderContainer'>
-      <div className={`ChallengeHeaderSideColor ${color}`} />
-      <h1 className='ChallengeHeaderTitle'>{difficulty}</h1>
-      <div className='Filter'>
-        <input type="text" placeholder='Filtro' className="InputWithIcon" maxLength="25" />
-        <span className="Icon">
-          <div className="FilterBar" />
-          <FontAwesomeIcon icon={faFilter} className='FilterIcon' />
-        </span>
+      <div className='ColumnLeftHeader'>
+        <div className={`ChallengeHeaderSideColor ${color}`} />
+        <h1 className='ChallengeHeaderTitle'>{difficulty}</h1>
       </div>
+      <div className='ColumnRightHeader'>
+        <div className="DifficultySelector">
+          <button
+            className={`DifficultyButton ${selectedDifficulty === 'Fácil' ? 'active' : ''}`}
+            onClick={() => onSelectDifficulty('Fácil')}
+          >
+            Fácil
+          </button>
+          <button
+            className={`DifficultyButton ${selectedDifficulty === 'Médio' ? 'active' : ''}`}
+            onClick={() => onSelectDifficulty('Médio')}
+          >
+            Médio
+          </button>
+          <button
+            className={`DifficultyButton ${selectedDifficulty === 'Difícil' ? 'active' : ''}`}
+            onClick={() => onSelectDifficulty('Difícil')}
+          >
+            Difícil
+          </button>
+        </div>
+      </div>
+
     </div>
   );
 };
